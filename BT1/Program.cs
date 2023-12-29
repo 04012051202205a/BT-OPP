82ed26d8a735736dc0e551dd74b8318665d237d5
ADMIN <ADMIN@PC306>
2023-12-29 09:51:01 +0700

    Add project files.

---
+++ BT1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT1
+{
+    class Rectangle
+    {
+        private double chieudai;
+        private double chieurong;
+        public Rectangle()
+        {
+            chieudai = 1.0;
+            chieurong = 2.0;
+
+        }
+        public Rectangle(double chieudai,double chieurong)
+        {
+            this.chieudai = chieudai;
+            this.chieurong = chieurong;
+        }
+        public void setchieudai(double chieudai)
+        {
+            this.chieudai = chieudai;
+        }
+        public double getchieudai()
+        {
+            return chieudai;
+        }
+        public void setchieurong(double chieurong)
+        {
+            this.chieurong = chieurong;
+        }
+        public double getchieurong()
+        {
+            return chieurong;
+        }
+        public double dientich()
+        {
+            return chieudai * chieurong;
+        }
+        public double chuvi()
+        {
+            return (chieurong + chieudai) *2;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r1;
+            Rectangle r2;
+            r1 = new Rectangle();
+            r2 = new Rectangle();
+            Console.Write(" Chieu dai hinh chu nhat = ");
+            double chieudai = double.Parse(Console.ReadLine());
+            Console.Write("Chieu rong hinh chu nhat = ");
+            double chieurong = double.Parse(Console.ReadLine());
+            Console.WriteLine("-------------------ket qua phep tinh la----------------");
+            Console.WriteLine("----------HINH CHU NHAT THU 1------------");
+            Console.WriteLine(" chieu dai {0}\n chieurong {1}\n dientich {2}\n chuvi {3}\n", r1.getchieudai(), r1.getchieurong(), r1.dientich(), r1.chuvi());
+            Console.WriteLine("----------HINH CHU NHAT THU 2------------");
+            Console.WriteLine(" chieu dai {0}\n chieurong {1}\n dientich {2}\n chuvi {3}\n", r2.getchieudai(), r2.getchieurong(), r2.dientich(), r2.chuvi());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BT2/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT2
+{
+    class Acount
+    {
+        private string ID;
+        private string Name;
+        private int Balance;
+        public Acount(string ID, string Name, int balance)
+        {
+            this.ID = ID;
+            this.Name = Name;
+            this.Balance = balance;
+        }
+        public string getID()
+        {
+            return ID;
+        }
+        public string getName()
+        {
+            return Name;
+
+        }
+        public int getBalance()
+        {
+            return Balance;
+        }
+        public void credit(int amount)
+        {
+            if (amount > 0)
+                Balance += amount;
+            else
+                Console.WriteLine("so tien khong hop le");
+
+        }
+        public void debit(int amount)
+        {
+            if (amount <= Balance)
+                Balance -= amount;
+            else
+                Console.WriteLine("so tien k du,rut tien khong thanh cong");
+        }
+        public void tranfer(Acount acc,int amount)
+        {
+            if (amount <= Balance)
+            {
+                Balance -= amount;
+                acc.Balance -= amount;
+            }
+            else
+                Console.WriteLine("so tien vuot qua so du");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int amount = 0;
+            Acount my = new Acount("999999", "Tran Van B", 200000000);
+            Console.Write("ID cua tai khoan ");
+            string ID = Console.ReadLine();
+            Console.Write("Ten tai khoan ");
+            string Name = Console.ReadLine();
+            Console.Write("so tien co trong tai khoan ");
+            int balance = int.Parse(Console.ReadLine());
+            Acount myfriend = new Acount(ID, Name, balance);
+            Console.Write("chuyen tien ");
+            amount = int.Parse(Console.ReadLine());
+            Console.Write("so tien muon rut la ");
+            amount = int.Parse(Console.ReadLine());
+            Console.WriteLine("-----------------Thong tin 2 tai khoan --------------");
+            Console.WriteLine("ID:{0},Name:{1},so du:{2}", my.getID()
+               , my.getName(), my.getBalance());
+            Console.WriteLine("ID:{0},Name:{1},so du:{2}", myfriend.getID()
+                , myfriend.getName(), myfriend.getBalance());
+            Console.ReadLine();
+            
+           
+            
+            
+        }
+    }
+}
